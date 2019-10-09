4d606a659cd40ca401050ab6fedd2ab214719270
Gianluca Serao <gianluca.serao.dj@gmail.com>
2019-10-09 14:50:37 +0200

    Removed the maven project

--- DB/Tables/building.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `building` (
-  `id_building` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `address` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_building`),
-  UNIQUE KEY `id_building_UNIQUE` (`id_building`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table Building. Room Booking Data Base';
--- DB/Tables/person.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `person` (
-  `id_person` varchar(10) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `lastname` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `role` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_person`),
-  UNIQUE KEY `id_person_UNIQUE` (`id_person`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table Person. Room Booking Data Base';
