61c15d1c8ce34ff67e82a802228c700805fbb893
Gianluca Serao <gianluca.serao.dj@gmail.com>
2019-10-13 23:43:52 +0200

    Directory rebuild

--- database/tables/belong.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `belong` (
-  `id_room` int(11) NOT NULL,
-  `id_building` int(11) NOT NULL,
-  PRIMARY KEY (`id_room`,`id_building`),
-  KEY `fk_idbuilding_idx` (`id_building`),
-  CONSTRAINT `fk_id_room` FOREIGN KEY (`id_room`) REFERENCES `room` (`id_room`),
-  CONSTRAINT `fk_idbuilding` FOREIGN KEY (`id_building`) REFERENCES `building` (`id_building`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- database/tables/building.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `building` (
-  `id_building` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `address` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_building`),
-  UNIQUE KEY `id_building_UNIQUE` (`id_building`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='Table Building. Room Booking Data Base';
