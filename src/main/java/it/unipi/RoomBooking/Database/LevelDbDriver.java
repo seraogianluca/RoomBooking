4c82d5efcba8019e3c45fddd061f8c53c69cf4d1
Gianluca Serao <49692424+seraogianluca@users.noreply.github.com>
2019-11-24 22:34:10 +0100

    Update LevelDbDriver.java

--- src/main/java/it/unipi/RoomBooking/Database/LevelDbDriver.java
+++ src/main/java/it/unipi/RoomBooking/Database/LevelDbDriver.java
@@ -102,7 +102,6 @@ public class LevelDbDriver {
 			levelDb = factory.open(new File(availablePath), options);
 			String key = "cla:" + roomId + ":available";
 			levelDb.put(bytes(key), bytes("f"));
-			System.out.println(key + " = f");
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		} finally {
@@ -116,10 +115,8 @@ public class LevelDbDriver {
 			levelDb.delete(bytes("cla:" + roomId + ":available"));
 			if (requestedSchedule.equals("m")) {
 				levelDb.put(bytes("cla:" + roomId + ":available"), bytes("a"));
-				System.out.println("cla:" + roomId + ":available = a");
 			} else {
 				levelDb.put(bytes("cla:" + roomId + ":available"), bytes("m"));
-				System.out.println("cla:" + roomId + ":available = m");
 			}
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
